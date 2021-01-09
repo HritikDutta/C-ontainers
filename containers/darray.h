2c5c176244e971e53b3438c35c557fcad24766de
ThreewayDude <dutta.hritik@gmail.com>
2021-01-09 13:44:54 +0530

    Some basic code restructuring to make it look "pretty"?

--- containers/darray.h
+++ containers/darray.h
@@ -11,6 +11,23 @@
 
 #define DArray(type) type*
 
+#define da_make(arr)                 da_make_impl((void**)&arr, DARRAY_START_CAP, sizeof(*arr))
+#define da_copy(dest, src)           da_copy_impl((void**)&dest, (void*)src, sizeof(*src))
+#define da_move(dest, src)           da_move_impl((void**)&dest, (void**)&src, sizeof(*src))
+#define da_free(arr)                 da_free_impl((void**)&arr)
+
+#define da_make_with_cap(arr, cap)   da_make_impl((void**)&arr, cap, sizeof(*arr))
+#define da_resize(arr, cap)          da_resize_impl((void**)&arr, cap, sizeof(*arr))
+
+#define da_size(arr)                 da_size_impl((void*)arr)
+#define da_cap(arr)                  da_cap_impl((void*)arr)
+
+#define da_push_back(arr, value)     da_push_back_impl(arr, value)
+#define da_pop_back(arr)             da_pop_back_impl(arr)
+#define da_insert(arr, index, value) da_insert_impl(arr, index, value)
+#define da_erase_at(arr, index)      da_erase_at_impl(arr, index)
+#define da_erase_swap(arr, index)    da_erase_swap_impl(arr, index)
+
 typedef struct
 {
     size_t cap;
@@ -28,22 +45,7 @@ void da_resize_impl(void** arr, size_t new_cap, size_t type_size);
 inline size_t da_size_impl(void* arr);
 inline size_t da_cap_impl(void* arr);
 
-#define da_make(arr)       da_make_impl((void**)&arr, DARRAY_START_CAP, sizeof(*arr))
-#define da_copy(dest, src) da_copy_impl((void**)&dest, (void*)src, sizeof(*src))
-#define da_move(dest, src) da_move_impl((void**)&dest, (void**)&src, sizeof(*src))
-#define da_free(arr)       da_free_impl((void**)&arr)
-
-#define da_make_with_cap(arr, cap) da_make_impl((void**)&arr, cap, sizeof(*arr))
-#define da_resize(arr, cap)        da_resize_impl((void**)&arr, cap, sizeof(*arr))
-
-#define da_size(arr) da_size_impl((void*)arr)
-#define da_cap(arr)  da_cap_impl((void*)arr)
-
-#define da_push_back(arr, value)     da_push_back_impl(arr, value)
-#define da_pop_back(arr)             da_pop_back_impl(arr)
-#define da_insert(arr, index, value) da_insert_impl(arr, index, value)
-#define da_erase_at(arr, index)      da_erase_at_impl(arr, index)
-#define da_erase_swap(arr, index)    da_erase_swap_impl(arr, index)
+#endif // DARRAY_H
 
 #ifdef DARRAY_IMPL
 
@@ -198,5 +200,3 @@ inline size_t da_cap_impl(void* arr)
     } while(0)
 
 #endif
-
-#endif // DARRAY_H
