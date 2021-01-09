65fa8265442495f6b4526dc725ab067a4690300b
ThreewayDude <dutta.hritik@gmail.com>
2021-01-09 13:39:17 +0530

    Finished implementation of DArray and fixed issues with copying and reallocation.

--- containers/darray.h
+++ containers/darray.h
@@ -25,19 +25,16 @@ void da_free_impl(void** arr);
 
 void da_resize_impl(void** arr, size_t new_cap, size_t type_size);
 
-// void da_insert_impl(void** arr, size_t index, void* val, size_t type_size);
-// void da_erase_at_impl(void** arr, size_t index, size_t type_size);
-// void da_erase_swap_impl(void** arr, size_t index, size_t type_size);
-
 inline size_t da_size_impl(void* arr);
 inline size_t da_cap_impl(void* arr);
 
-#define da_make(arr)       da_make_impl((void**)&arr, 0, sizeof(*arr))
+#define da_make(arr)       da_make_impl((void**)&arr, DARRAY_START_CAP, sizeof(*arr))
 #define da_copy(dest, src) da_copy_impl((void**)&dest, (void*)src, sizeof(*src))
 #define da_move(dest, src) da_move_impl((void**)&dest, (void**)&src, sizeof(*src))
 #define da_free(arr)       da_free_impl((void**)&arr)
 
-#define da_resize(arr, cap) da_resize_impl((void**)&arr, cap, sizeof(*arr))
+#define da_make_with_cap(arr, cap) da_make_impl((void**)&arr, cap, sizeof(*arr))
+#define da_resize(arr, cap)        da_resize_impl((void**)&arr, cap, sizeof(*arr))
 
 #define da_size(arr) da_size_impl((void*)arr)
 #define da_cap(arr)  da_cap_impl((void*)arr)
@@ -45,7 +42,7 @@ inline size_t da_cap_impl(void* arr);
 #define da_push_back(arr, value)     da_push_back_impl(arr, value)
 #define da_pop_back(arr)             da_pop_back_impl(arr)
 #define da_insert(arr, index, value) da_insert_impl(arr, index, value)
-#define da_erase_at(arr, index)      da_erase_at(arr, index)
+#define da_erase_at(arr, index)      da_erase_at_impl(arr, index)
 #define da_erase_swap(arr, index)    da_erase_swap_impl(arr, index)
 
 #ifdef DARRAY_IMPL
@@ -65,33 +62,39 @@ void da_make_impl(void** arr, size_t cap, size_t type_size)
     da->cap  = cap;
     da->size = 0;
 
-    *arr = da + sizeof(DA_Internal);
+    *arr = da + 1;
 }
 
 void da_copy_impl(void** dest, void* src, size_t type_size)
 {
+    hd_assert(src != NULL);
     DA_Internal* src_da = da_data(src);
     size_t byte_size = src_da->cap * type_size + sizeof(DA_Internal);
-    DA_Internal* new_da = (DA_Internal*) realloc(da_data(*dest), byte_size);
-    hd_assert(new_da != NULL);
     
-    new_da->cap  = src_da->cap;
-    new_da->size = src_da->size;
+    DA_Internal* dest_da;
+    if (*dest) dest_da = (DA_Internal*) realloc(da_data(*dest), byte_size);
+    else       dest_da = (DA_Internal*) malloc(byte_size);
 
-    *dest = new_da + sizeof(DA_Internal);
-    memcpy(*dest, src, src_da->size * type_size);   
+    hd_assert(dest_da != NULL);
+    
+    size_t filled_byte_size = src_da->size * type_size + sizeof(DA_Internal);
+    memcpy(dest_da, src_da, filled_byte_size);
+
+    *dest = dest_da + 1;
 }
 
 void da_move_impl(void** dest, void** src, size_t type_size)
 {
+    hd_assert(*src != NULL);
     DA_Internal* dest_da = da_data(*src);
     
     *src  = NULL;
-    *dest = dest_da + sizeof(DA_Internal);
+    *dest = dest_da + 1;
 }
 
 void da_free_impl(void** arr)
 {
+    hd_assert(*arr != NULL);
     DA_Internal* da = da_data(arr);
     free(da);
     arr = NULL;
@@ -100,12 +103,12 @@ void da_free_impl(void** arr)
 
 void da_resize_impl(void** arr, size_t new_cap, size_t type_size)
 {
-    DA_Internal* da;
-    size_t size = 0, cap = new_cap;
+    DA_Internal* da = NULL;
+    size_t size = 0;
     
     if (*arr)
     {
-        da   = da_data(arr);
+        da   = da_data(*arr);
         size = da->size;
     }
 
@@ -113,7 +116,9 @@ void da_resize_impl(void** arr, size_t new_cap, size_t type_size)
     DA_Internal* new_da = (DA_Internal*) realloc(da, byte_size);
     
     new_da->size = size;
-    new_da->cap  = cap;
+    new_da->cap  = new_cap;
+
+    *arr = new_da + 1;
 }
 
 inline size_t da_size_impl(void* arr)
@@ -137,34 +142,59 @@ inline size_t da_cap_impl(void* arr)
         DA_Internal* da = da_data(arr);                             \
         size_t size = da->size, cap = da->cap;                      \
                                                                     \
-        if (cap == 0) da_resize(arr, DARRAY_START_CAP);             \
+        if (cap == 0)    da_resize(arr, DARRAY_START_CAP);          \
         if (size >= cap) da_resize(arr, DARRAY_GROWTH_RATE * cap);  \
                                                                     \
-        arr[size++] = value;                                        \
+        arr[da_data(arr)->size++] = value;                          \
     } while (0)
 
 #define da_pop_back_impl(arr) \
-    do {                                \
-        hd_assert(arr != NULL);         \
-        DA_Internal* da = da_data(arr); \
-        size_t size = da->size;         \
-                                        \
-        if (size > 0) size--;           \
+    do {                                    \
+        hd_assert(arr != NULL);             \
+        DA_Internal* da = da_data(arr);     \
+        size_t size = da->size;             \
+                                            \
+        if (size > 0) da->size--;           \
     } while(0)
 
 #define da_insert_impl(arr, index, value) \
-    do {\
-        \
+    do {                                                                \
+        hd_assert(arr != NULL);                                         \
+        DA_Internal* da = da_data(arr);                                 \
+        size_t size = da->size, cap = da->cap;                          \
+                                                                        \
+        if (cap == 0)        da_resize(arr, DARRAY_START_CAP);          \
+        if (size + 1 >= cap) da_resize(arr, DARRAY_GROWTH_RATE * cap);  \
+                                                                        \
+        for (int i = size; i > index; i--)                              \
+            arr[i] = arr[i - 1];                                        \
+                                                                        \
+        arr[index] = value;                                             \
+        da_data(arr)->size++;                                           \
     } while(0)
 
 #define da_erase_at_impl(arr, index) \
-    do {\
-        \
+    do {                                        \
+        hd_assert(arr != NULL);                 \
+        DA_Internal* da = da_data(arr);         \
+        size_t size = da->size;                 \
+        hd_assert(size > 0 && index < size);    \
+                                                \
+        for (int i = index; i < size - 1; i++)  \
+            arr[i] = arr[i + 1];                \
+                                                \
+        da->size--;                             \
     } while(0)
 
 #define da_erase_swap_impl(arr, index) \
-    do {\
-        \
+    do {                                        \
+        hd_assert(arr != NULL);                 \
+        DA_Internal* da = da_data(arr);         \
+        size_t size = da->size;                 \
+        hd_assert(size > 0 && index < size);    \
+                                                \
+        arr[index] = arr[size - 1];             \
+        da->size--;                             \
     } while(0)
 
 #endif
--- main.c
+++ main.c
@@ -3,22 +3,36 @@
 #define DARRAY_IMPL
 #include "containers/darray.h"
 
+#define LOG_INT(x) printf("%s: %d\n", #x, x)
+
 int main()
 {
-    DArray(int) a;
-    da_make_with_size(a, 5);
+    DArray(int) arr = NULL;
+    da_make(arr);
 
-    DArray(int) arr = make_darray(5);
-    da_insert(arr, 0, 4);
+    printf("arr :: size: %zd, cap: %zd\n", da_size(arr), da_cap(arr));
+
+    da_push_back(arr, 4);
     da_push_back(arr, 3);
+    da_push_back(arr, 5);
+    da_push_back(arr, 7);
+    da_push_back(arr, 8);
     
+    da_pop_back(arr);
+
+    printf("arr :: size: %zd, cap: %zd\n", da_size(arr), da_cap(arr));
+
+    for (int i = 0; i < da_size(arr); i++)
+        printf("arr[%d] :: %d\n", i, arr[i]);
+
     DArray(int) dest = NULL;
     da_copy(dest, arr);
 
+    printf("\ndest :: size: %zd, cap: %zd\n", da_size(dest), da_cap(dest));
+
     for (int i = 0; i < da_size(dest); i++)
-    {
-        printf("%d\n", dest[i]);
-    }
+        printf("dest[%d] :: %d\n", i, dest[i]);
 
     da_free(arr);
+    da_free(dest);
 }
